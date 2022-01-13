cc3b8fd96b87f9b26d171894ab23d0aa61674158
17Moons <vaibhav.singh.cd.eee20@itbhu.ac.in>
2022-01-13 18:29:14 +0530

    Sign in to Log in

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -54,7 +54,7 @@ function Homepage() {
     <div className="Homepage">
 
     <div className="link-homepage-text">
-      <h3 className="links_homepage sign"> <Link to="/signup" style={{ textDecoration: 'none' ,color: 'inherit' }} > Sign Up</Link></h3>
+      <h3 className="links_homepage sign"> <Link to="/signup" style={{ textDecoration: 'none' ,color: 'inherit' }} >Login</Link></h3>
       <h3 className="links_homepage about"><Link to="/aboutus" style={{ textDecoration: 'none' ,color: 'inherit' }} >About Us</Link></h3>
 
     </div>
@@ -64,7 +64,7 @@ function Homepage() {
     <h4 className="description">Now that your eyes are open, make the sun jealous with your burning
 passion to start the day. Make the sun jealous or stay in bed.</h4>
     </div>
-    <Link to="/homepae" style={{ textDecoration: 'none' ,color: 'inherit' }} > Sign Up</Link>
+
 
   </div>
 

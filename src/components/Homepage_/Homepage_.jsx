91d57e0619795cbc4b1f59e0355e0f3221e6e7b4
17Moons <vaibhav.singh.cd.eee20@itbhu.ac.in>
2022-01-22 12:19:22 +0530

    login to close and then back

--- src/components/Homepage_/Homepage_.jsx
+++ src/components/Homepage_/Homepage_.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.css';
 import Signup from "../Signup_/Signup/Signup_";
 import { useState } from 'react';
 import './Homepage_css_.css';
@@ -8,66 +7,62 @@ import './Homepage_css_.css';
 
 
 
+
   const Home = () => {
+
 
   const [name, setName] = useState('Login');
 
-  const handleClick = () => {
+
+
+
+
+
+
+
+
+  function handleClick () {
+
 
 
          document.getElementById('menu-toggle').addEventListener('click', (e)=> {
-           setName('Close');
+
              e.stopPropagation();
+                 var x= document.getElementById('menu-toggle').innerHTML;
+
+                 if(x=='Login')
+                 {
+                   setName('Close');
+                 }
+                 else
+                 {
+                   setName('Login');
+                 }
+
+             console.log("clicked");
              document.getElementById('sidebar').classList.toggle('visible');
              document.getElementById('sidebar-content').classList.toggle('d-none');
              document.getElementById('content').classList.toggle('shift');
-             document.getElementById('overlay').classList.toggle('overlay');
-
-         })
-
-         document.getElementById('overlay').addEventListener('click', () => {
-           setName('login');
-             document.getElementById('sidebar').classList.remove('visible');
-             setTimeout(()=>{
-                 document.getElementById('sidebar-content').classList.add('d-none');
-             },400);
-             document.getElementById('content').classList.remove('shift');
-             document.getElementById('overlay').classList.remove('overlay');
-             closeModal();
-
-         })
-
-         document.getElementById('get-started').addEventListener('click', () => {
-
-             document.getElementById('sign-up-modal').classList.add('modal-visible');
-             setTimeout(()=>{
-                 document.getElementById('modal-content').classList.remove('d-none');
-             },300);
-             document.getElementById('overlay').classList.add('overlay');
 
 
          })
-
-         document.getElementById('close-modal').addEventListener('click', closeModal);
-
-         function closeModal()
-         {
-             name='Login';
-             document.getElementById('overlay').classList.remove('overlay');
-             document.getElementById('sign-up-modal').classList.remove('modal-visible');
-             document.getElementById('modal-content').classList.add('d-none');
-
-         }
+       }
 
 
-  }
+
+
+
+
+
 
   return (
 
     <div className="Homepage" id="content">
    <div className="basepage" id="content">
     <div className="link-homepage-text" id="content">
-      <h3 className="links_homepage_sign" id="menu-toggle" onClick={ handleClick }>{name}</h3>
+      <h3 className="links_homepage_sign" id="menu-toggle" onClick=  {() => {
+                  handleClick();
+                }}>{name}</h3>
 
     </div>
 
